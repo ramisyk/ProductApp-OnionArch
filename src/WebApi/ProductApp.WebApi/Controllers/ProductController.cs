2db05e03443d744b75f07866a7fdbe46b47fb306
Ramis Yuksel <ramisyk97@gmail.com>
2022-04-02 17:07:01 +0300

    controller is added

---
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await productRepository.GetAllAsync();
+            allList.Select(i => new ProductViewDto()
+            { Id = i.Id, Name = i.Name }).ToList();
+            return Ok(allList);
+        }
+    }
+}
